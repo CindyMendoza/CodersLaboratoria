07707f422bac0c8563837d3f4b03bd848882df69
cindy <mendozaic@hotmail.com>
2017-04-07 21:06:25 -0500

    section images finished yeahhgit add .

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,68 +33,21 @@ document.getElementById('toggle').addEventListener("click",function(event){
 */
 var showImage = document.getElementById('showImage');
 var docFrac = document.createDocumentFragment();
-var namesCoders = ["Marilu Llamocca Junco",
-"RUTH MERY CARDENAS PEREZ",
-"Maria Lourdes Vilchez Palomino",
-"Glisse Lisbeth Jorge Malca",
-"Ana de los Angeles Durand Calle",
-"Ruth Abigail Salvador Zorrilla",
-"Leslie Ashley AvendaÃ±o Lopez",
-"Flor de Cantuta Tello Sarmiento",
-"Miriam Peralta CastaÃ±eda",
-"Fiorella Quispe Arotinco",
-"Annia Jasenka Flores PÃ©rez",
-"LEIDY ROSMERY MALDONADO CHAUCA",
-"Rosario Felix Tasayco",
-"Liliana Fiorela PeÃ±a Cueva",
-"Miriam Gisella Mendoza Pilco",
-"Flor Marina Condori Huamani",
-"Naomi Villanueva Ajito",
-"Luz Milagros Gutierrez Malca",
-"Karin Alejo Campos",
-"Ingrid Michelle More Flores",
-"Monica Fiorella Cisneros Romero",
-"Betsi Ana Loayza Flores",
-"Mariel del Rosario GarcÃ­a Lara",
-"Ruth Eliana Silva Castillo",
-"Erika Diana Vidal Capillo",
-"Angie Gesenia Condor Macuri",
-"Stephanie Pamela Hiyagon Arroyo",
-"Grecia Leticia Rayme Pumacayo",
-"Cindy Deisy Mendoza Ibarra",
-"Mitchell RodrÃ­guez LeÃ³n",
-"Maria Grecia Cutipa GonzÃ¡les",
-"Geraldine Chauca Escobar",
-"Mary katherine Castillo Baygorrea",
-"Nadia Cuadros Prieto",
-"Elizabeth Condori Gonzales",
-"Rocio Emma Tapia Alva",
-"Neiza Luz NuÃ±ez Pfoccoalata",
-"ARANTZA BURGA VALDERRAMA",
-"Schelsen Brilly Majuan Lopez",
-"Sandra Vanessa SolÃ³rzano Vargas",
-"Yelitza Choque Rivera",
-"Katherine Cecilia Ortega Ramos",
-"Maricarmen Rojas Tinco",
-"Aslheys Ariana Nicholle Cabana Cahuana",
-"Nathaly Grace Pacheco Asto",
-"Janine Vega Uribe",
-"WENDY SHIRLEY REYES SANTANA",
-"Milagros Gonzales Ore",
-"Jenny Mishel Velasquez Mamani",
-"Dana Lissete Franco Pittman",
-"Maria Nakarid Jave Sagastegui",
-"AYDA Sulca Urpay",
-"Magali Zambrano Neri",
-"Flor Retamozo Marengo"];
+var namesCoders=["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia Huamán","Miriam Mendoza","Elizabeth Condori","Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez","Nadia Cuadros","Michell More","Marilu Llamocca","Mariel Garcia","Fiorella Cisneros","Geraldine Chauca","Yellitza Rivera","Stephanie Hiyagon","Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
+"Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello","Leslie Avendaño","Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor","Maricarmen Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez","Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo","Liliana Peña","Ruth Salvador","Maribel Sevilla","Wendy Reyes","Cinthia",
+"Maria Grecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez","Sandra Solorzano"];
 for ( var i = 1; i < 55; i++){
   var img = document.createElement("img");
   var div = document.createElement("div");
-  var txtName = document.createTextNode("")
+  var spantxt = document.createElement("span");
+  var txtName = document.createTextNode(namesCoders[i-1]);
   img.setAttribute("src","assets/img/students/"+i+".png");
   img.setAttribute("class","img");
   div.setAttribute("class","containerImg");
+  spantxt.setAttribute("class","txt");
+  spantxt.appendChild(txtName);
   div.appendChild(img);
+  div.appendChild(spantxt);
   docFrac.appendChild(div);
 }
 showImage.appendChild(docFrac);
