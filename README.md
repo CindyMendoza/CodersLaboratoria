5f16b5e6dec63c965bb5bc92d69de15778237931
cindy mendoza <mendozaic@hotmail.com>
2017-10-12 10:25:02 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+##Coders de laboratoria
+Maquetado web, ejercicio realizado durante el bootcamp de Laboratoria Sprint 3, tema CSS e Interaciones.
+Se muestra fotos de la 6ta promocion la cual lleva el nombre de LOVELACE WARRIORS, tambien se realiza un menu conocido como el menu hamburguesa con puro css3 ademas de efectos con scroll.
+Puedes revisar el proyecto aqui.
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,41 @@
-var lastScrollTop = 0;
+//Menu hamburguesa
+document.getElementById("toggle").addEventListener("click", function(){
+  document.getElementById("nav-header").classList.toggle("open");
+});
+//nav con scroll
+var c = 151;
 window.addEventListener("scroll", function(){
    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-   if (currentScroll > lastScrollTop){
-     document.getElementById('nav-background').style.display="none";
-     document.getElementById("nav-header").classList.remove("header-in");
-     document.getElementById("nav-header").style.opacity = 0;
-   } else {
-     document.getElementById('nav-background').style.display="table";
-     document.getElementById("nav-header").classList.add("header-in");
-     document.getElementById("nav-header").classList.add("solid");
-     document.getElementById("nav-header").style.opacity = 1;
-     if(currentScroll<=3){
-       document.getElementById("nav-header").classList.remove("solid");
-       document.getElementById("nav-header").classList.remove("header-in");
-     }
+   console.log(currentScroll);
+   if (currentScroll >= 0 && currentScroll <= 150){
+     document.getElementById('nav-header').style.display="block";
+     document.getElementById('nav-header').classList.remove('scroll');
+     document.getElementsByClassName('menu')[0].classList.remove('scroll-menu');
+   } else  if (currentScroll>c) {
+     document.getElementById('nav-header').style.display="none";
    }
-   lastScrollTop = currentScroll;
+   else {
+     document.getElementById('nav-header').style.display="block";
+     document.getElementById('nav-header').classList.add('scroll');
+     document.getElementsByClassName('menu')[0].classList.add('scroll-menu');
+   }
+   c=currentScroll;
+  //  }else{
+  //    document.getElementById('nav-header').style.display="block";
+  //    document.getElementById('nav-header').classList.add('scroll');
+  //    document.getElementsByClassName('menu')[0].classList.add('scroll-menu');
+  //  }
+
 }, false);
-document.getElementById("toggle").addEventListener("click", function(event){
-  event.preventDefault();
-  document.getElementById("nav-header").classList.toggle("open");
-  document.getElementById('body').classList.toggle("overflow-hidden");
-});
 
-
+//carga fotos coders
 var showImage = document.getElementById('showImage');
 var docFrac = document.createDocumentFragment();
 var namesCoders=["Fiorella Quispe","Lourdes Vilchez","Rossmery Maldonado","Yessenia Huamán","Miriam Mendoza","Elizabeth Condori","Arantza Burga","Grecia Rayme","Janine Vega","Rosario Felix","Daguiana Revoredo","Jenny Velasquez","Nadia Cuadros","Michell More","Marilu Llamocca","Mariel Garcia","Fiorella Cisneros","Geraldine Chauca","Yellitza Rivera","Stephanie Hiyagon","Emma Tapia","Danna Franco","Flor Retamozo","Nathaly Pacheco",
 "Ericka Vidal","Katherine Ortega","Brilly Majuan","Flor Tello","Leslie Avendaño","Cindy Mendoza","Annia Flores","Betsi Loayza","Aida Sulca","Milagros Gutierrez","Nakarid Jave","Angie Cóndor","Maricarmen Rojas","Ariana Cabana","Flor Condori","Mitch Rodríguez","Naomi Villanueva","Mari Castillo","Miriam Peralta","Karin Alejo","Liliana Peña","Ruth Salvador","Maribel Sevilla","Wendy Reyes","Cinthia",
 "Maria Grecia Cutipa","Ana Durand","Glisse Jorge","Neiza Nuñez","Sandra Solorzano"];
 window.addEventListener("load",function(){
-  for ( var i = 1; i < 55; i++){
+  for ( var i = 1; i < namesCoders.length; i++){
     var img = document.createElement("img");
     var div = document.createElement("div");
     var spantxt = document.createElement("span");
